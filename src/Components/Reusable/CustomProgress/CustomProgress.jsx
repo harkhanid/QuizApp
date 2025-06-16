f1dfba9e11b063402d71f9e201c75b0f3633c394
Dharmik <harkhani.d@northeastern.edu>
2025-06-15 23:43:51 -0400

    Added documentation

---
+++ src/Components/Reusable/CustomProgress/CustomProgress.jsx
@@ -0,0 +1,13 @@
+/**
+ * CustomProgress Component
+ * A customizable progress bar component that displays completion status.
+ * Used to show quiz progress and other completion metrics.
+ * 
+ * @component
+ * @param {Object} props - Component props
+ * @param {number} props.value - Current progress value (0-100)
+ * @param {string} [props.color] - Optional color for the progress bar
+ * @param {string} [props.className] - Optional CSS class name
+ * @returns {React.ReactNode} The rendered progress bar component
+ */
+ 
